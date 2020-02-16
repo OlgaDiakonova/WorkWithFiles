470a72ffe9f08a72eae41365b3c016508b4dd7e3
olga <odiakonova@mysenca.ca>
2020-02-16 13:19:11 -0500

    used Path instead of File in variable declaration

--- src/MyFile.java
+++ src/MyFile.java
@@ -4,12 +4,13 @@ import java.nio.file.Paths;
 
 public class MyFile {
 
-    public static final Path ABS_PATH = Paths.get("C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT.dat.txt");
+    public static final Path FROM_PATH = Paths.get("C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT.dat.txt");
+    public static final Path TO_PATH = Paths.get("C:\\Users\\admin\\Desktop\\Go2ITcopy.dat.txt");
 
     public static void main(String[] args) throws IOException {
 
-        File myFile = new File(String.valueOf(ABS_PATH));
-        File copyFile = new File("C:\\Users\\admin\\Desktop\\Go2ITcopy.dat.txt");
+        File myFile = FROM_PATH.toFile();
+        File copyFile = TO_PATH.toFile();
         if(myFile.exists()){
             System.out.println("File exists!");
             try(FileReader fileReader = new FileReader(myFile);
