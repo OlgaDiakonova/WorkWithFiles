ce154757a8d65460cfe0adc462c28dec2c284125
olga <odiakonova@mysenca.ca>
2020-02-18 21:07:18 -0500

    add code for networking(download pdf)

--- src/MyFile.java
+++ src/MyFile.java
@@ -1,6 +1,9 @@
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -42,15 +45,23 @@ public class MyFile {
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\downloadedFromW3School.css")))){
             String line;
             while((line = reader.readLine()) != null){
-                writer.write(line);
+                writer.write(line + "\n");
             }
         }
 
     }
 
-    public static void createTreeCopiesOfFile() throws IOException {
-        Path filePath = Paths.get("C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT.dat.txt");
+    public static void downloadPDF() throws IOException {
 
+        URL webAddress = new URL("http://www.ukrstat.gov.ua/express/expr2019/03/38.pdf");
+        ReadableByteChannel rbc = Channels.newChannel(webAddress.openStream());
+        FileOutputStream fos = new FileOutputStream("src/test.pdf");
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+    }
+
+    public static void createThreeCopiesOfFile() throws IOException {
+        Path filePath = Paths.get("C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT.dat.txt");
         try (BufferedReader reader =
                      new BufferedReader(new InputStreamReader(new FileInputStream(filePath.toFile())))
         ) {
@@ -59,10 +70,11 @@ public class MyFile {
             BufferedWriter writer = null;
             for (int i = 1; i <= 3; i++) {
                 Path copyFile = Paths.get("C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT_copy"+i+".dat.txt");
+                Files.deleteIfExists(copyFile);
                 try {
-                    writer = Files.newBufferedWriter(copyFile, Charset.forName("UTF-8"), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+                    writer = Files.newBufferedWriter(copyFile, Charset.forName("UTF-8"), StandardOpenOption.APPEND, StandardOpenOption.CREATE_NEW);
                     while ((line = reader.readLine()) != null) {
-                        writer.write(line);
+                        writer.write(line + "\n");
                     }
                     System.out.println("File is saved");
                 } finally {
@@ -73,10 +85,30 @@ public class MyFile {
         }
     }
 
+    public static void copyThreeFiles() throws IOException {
+        Path file1 = Paths.get ( "C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT1.dat.txt");
+        Path file2 = Paths.get ( "C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT2.dat.txt");
+        Path file3 = Paths.get ( "C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT3.dat.txt" );
+        try (BufferedReader inStream = Files.newBufferedReader ( Paths.get ( "C:\\Users\\admin\\eclipse-workspace\\WorkingWithFiles\\src\\Go2IT.dat.txt" ) );
+             BufferedWriter bw1 = new BufferedWriter ( new FileWriter ( file1.toFile ( ) ) );
+             BufferedWriter bw2 = new BufferedWriter ( new FileWriter ( file2.toFile ( ) ) );
+             BufferedWriter bw3 = new BufferedWriter ( new FileWriter ( file3.toFile ( )  ) );
+        ) {
+            String line;
+            while (( line = inStream.readLine ( ) ) != null) {
+                bw1.write ( line + "\n" );
+                bw2.write ( line + "\n" );
+                bw3.write ( line + "\n" );
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 //        copyToFile();
           readFileFromGoogle();
-//        createTreeCopiesOfFile();
+//        createThreeCopiesOfFile();
+//        copyThreeFiles();
+        //downloadPDF();
 
     }
 }
